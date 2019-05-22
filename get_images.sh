63df17c263cd694d4d15ca3a388bc94c1c180ba1
Woojoong Kim <woojoong.m.kim@gmail.com>
2019-05-21 22:22:46 -0500

    Add the script to get all docker images

--- get_images.sh
+++ get_images.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# log in first
+docker login
 
+# get local cdn image which is nginx image
+docker pull woojoong/nginx:onf-video-v3
+
+# get remote cdn image - vlc image
+docker pull woojoong/mwc-cdn:remote-v3
+
+# get remote cdn image - wowza which will be a legacy image
+docker pull woojoong/wowza:latest

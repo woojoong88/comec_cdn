8f28ddf06b9a8b3e6c8167c0c7770f814b89f886
Woojoong Kim <woojoong.m.kim@gmail.com>
2019-05-21 22:43:12 -0500

    update get image script

--- get_images.sh
+++ get_images.sh
@@ -11,3 +11,6 @@ docker pull woojoong/mwc-cdn:remote-v3
 
 # get remote cdn image - wowza which will be a legacy image
 docker pull woojoong/wowza:latest
+
+# get remote cdn image - antmedia which will be a new image
+docker pull woojoong/antmedia:1.7.0
